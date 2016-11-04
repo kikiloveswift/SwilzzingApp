17569f2cad92269777a97ad3f854da29b36832ae
kingleekong <kong@konglee.local>
2016-11-04 22:11:46 +0800

    Initial Commit

---
+++ SwilzzingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwilzzingApp
+//
+//  Created by kong on 16/11/4.
+//  Copyright © 2016年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwilzzingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwilzzingApp
+//
+//  Created by kong on 16/11/4.
+//  Copyright © 2016年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
